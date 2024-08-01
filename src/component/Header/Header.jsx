57dcc82ab8d3b0344711fe4a89ad4f51b86ea4ba
darrk-heart <prospaul999@gmail.com>
2024-08-01 14:12:07 +0000

    working

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+
+function Header() {
+  return (
+    <div>
+      <div>Shortly</div>
+      <FaBars />
+    </div>
+  );
+}
+
+export default Header;
