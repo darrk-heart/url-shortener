1afc3600279f97f9edda165b847eec464baad96a
darrk-heart <prospaul999@gmail.com>
2024-08-01 14:27:01 +0000

    working

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
+import styles from "./Header.module.css";
+import illustration from "../../assets/illustration-working.svg";
 
 function Header() {
   return (
-    <div>
-      <div>Shortly</div>
-      <FaBars />
-    </div>
+    <>
+      <div className={styles.head}>
+        <div>Shortly</div>
+        <FaBars className={styles.fabar} />
+      </div>
+      <div>
+        <div>
+          <img src={illustration} alt="illustration image" />
+        </div>
+        <div>
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <p>Get Started</p>
+        </div>
+      </div>
+    </>
   );
 }
 
