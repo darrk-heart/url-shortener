c7b9898fbf98f596cc88ddb0f4ae26a86618e9cc
darrk-heart <prospaul999@gmail.com>
2024-08-06 10:07:20 +0000

    customizing page

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import brand from "../../assets/icon-brand-recognition.svg";
+import details from "../../assets/icon-detailed-records.svg";
+import customize from "../../assets/icon-fully-customizable.svg";
 
 function Footer() {
   return (
@@ -11,25 +14,25 @@ function Footer() {
           statistics dashboard
         </p>
       </div>
-      <div>
-        <div>
-          <img />
+      <div className={styles.container}>
+        <div className={styles.brands}>
+          <img src={brand} alt="brands recognition logo" />
           <h3>Brand Recognition</h3>
           <p>
             Boost your brand recognition with each click. Generic links don't
             mean a thing. Branded links help instill confidence in your content.
           </p>
         </div>
-        <div>
-          <img />
+        <div className={styles.records}>
+          <img src={details} alt="detail records logo" />
           <h3>Detailed Records</h3>
           <p>
             Gain insights into who is clicking your links. Knowing when and
             where people engage with your content helps inform better decisions.
           </p>
         </div>
-        <div>
-          <img />
+        <div className={styles.customize}>
+          <img src={customize} alt="fully customizable" />
           <h3>Fully Customizable</h3>
           <p>
             Improve brand awareness and content discovery through customizable
