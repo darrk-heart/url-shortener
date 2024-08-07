227a6a361de11f1ea772a18f14f06ff0c558ad1a
darrk-heart <prospaul999@gmail.com>
2024-08-07 11:50:14 +0000

    working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
+import Beneath from "./component/Beneath/Beneath";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <Beneath />
     </div>
   );
 }
---
+++ src/component/Beneath/Beneath.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+
+function Beneath() {
+  return (
+    <>
+      <div>
+        <h2>Boost your links today</h2>
+        <span>Get Started</span>
+      </div>
+      <div>
+        Shortly
+        <div>
+          <h4>Features</h4>
+          <p>Link Shortening</p>
+          <p>Branded Links</p>
+          <p>Analytics</p>
+        </div>
+        <div>
+          <h4>Resources</h4>
+          <p>Blog</p>
+          <p>Developers</p>
+          <p>Support</p>
+        </div>
+        <div>
+          <h4>Company</h4>
+          <p>About</p>
+          <p>Our Team</p>
+          <p>Careers</p>
+          <p>Contact</p>
+        </div>
+        <div>
+          <FaFacebook />
+          <FaTwitter />
+          <FaPinterest />
+          <BsInstagram />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Beneath;
