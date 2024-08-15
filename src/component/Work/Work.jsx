fa88d6d5b0a998b0478e24df27c077241eb26330
darrk-heart <prospaul999@gmail.com>
2024-08-15 11:36:07 +0000

    working

--- src/component/Work/Work.jsx
+++ src/component/Work/Work.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Work.module.css";
 
 function Work() {
+  const [inputValue, setInputValue] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const handleShortClick = () => {
+    if (inputValue === "") {
+      setHasError(true);
+    } else {
+      setHasError(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    if (e.target.value != "") {
+      setHasError(false);
+    }
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.display}>
-        <div className={styles.text}>
-          <input type="text" placeholder="Shorten a link here..." />
+        <div
+          className={styles.text}
+          style={{ border: hasError ? "2px solid red" : "2px solid white" }}
+        >
+          <input
+            type="text"
+            placeholder="Shorten a link here..."
+            onChange={handleInputChange}
+          />
         </div>
-        <div className={styles.label}>Shorten It!</div>
+        <span
+          className={styles.error}
+          style={{ display: hasError ? "block" : "none" }}
+        >
+          Please add a link
+        </span>
+        <div onClick={handleShortClick} className={styles.label}>
+          Shorten It!
+        </div>
       </div>
     </div>
   );
